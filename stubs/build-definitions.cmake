f35aed9ca42a83fe7ed69aca5c455ea02b0274b0
sectorzero <sndshd@gmail.com>
2014-07-27 20:56:09 -0700

    build-flags.cmake

---
+++ stubs/build-definitions.cmake
@@ -0,0 +1,2 @@
+set(HEADER_FILE_EXTENSIONS "h;hpp;inc" CACHE STRING "file extension of header files.")
+set(SOURCE_FILE_EXTENSIONS "cpp;c" CACHE STRING "file extension of source files.")
---
+++ stubs/build-flags.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++")
+add_definitions(-g -O2 -Wall)
