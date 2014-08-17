d6708a7ce545ad153b03f6808238ce5fbb9c34fa
sectorzero <sndshd@gmail.com>
2014-08-17 07:15:59 -0700

    Changed DOWNLOAD step of ExternalProjects
    - For GoogleProtobuf and GoogleTest
    - Changed to use custom download and into private location
    - File will get copied only if different

--- stubs/opt-googleprotobuf.cmake
+++ stubs/opt-googleprotobuf.cmake
@@ -1,131 +1,131 @@
 # >> --------- Google ProtoBuf ------------
 include(ExternalProject)
 
+set(GOOGLE_PROTOBUF_PROJ_NAME "GoogleProtobuf")
+set(GOOGLE_PROTOBUF_PREFIX_DIR "${CMAKE_BINARY_DIR}/${GOOGLE_PROTOBUF_PROJ_NAME}")
+
 set(GOOGLE_PROTOBUF_VERSION "protobuf-2.5.0.tar.gz")
 set(GOOGLE_PROTOBUF_VERSION_SHA1 "7f6ea7bc1382202fb1ce8c6933f1ef8fea0c0148")
 set(GOOGLE_PROTOBUF_VERSION_URL "https://protobuf.googlecode.com/files/${GOOGLE_PROTOBUF_VERSION}")
 
-if(OPT_PROTOBUF)
-    
-    set(GOOGLE_PROTOBUF_PROJ_NAME "GoogleProtobuf")
-    set(GOOGLE_PROTOBUF_PREFIX_DIR "${CMAKE_BINARY_DIR}/${GOOGLE_PROTOBUF_PROJ_NAME}")
-    
-    download_and_install_file(
-        ${GOOGLE_PROTOBUF_VERSION}
-        ${GOOGLE_PROTOBUF_VERSION_URL}
-        ${GOOGLE_PROTOBUF_VERSION_SHA1}
-        "${CMAKE_SOURCE_DIR}/.antilag-repo"
-        )
-    
-    set(GOOGLE_PROTOBUF_VERSION_URL_REPO "file://${CMAKE_SOURCE_DIR}/.antilag-repo/${GOOGLE_PROTOBUF_VERSION}")
-    message(STATUS ${GOOGLE_PROTOBUF_VERSION_URL_REPO})
-    
-    ExternalProject_Add(
-        ${GOOGLE_PROTOBUF_PROJ_NAME}
-        PREFIX  ${GOOGLE_PROTOBUF_PREFIX_DIR}
-        DOWNLOAD_DIR ${GOOGLE_PROTOBUF_PREFIX_DIR}/repo
-        URL ${GOOGLE_PROTOBUF_VERSION_URL_REPO}
-        URL_HASH SHA1=${GOOGLE_PROTOBUF_VERSION_SHA1}
-        CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --disable-shared
-        LOG_DOWNLOAD ON
-        LOG_UPDATE ON
-        LOG_CONFIGURE ON
-        LOG_BUILD ON
-        LOG_TEST ON
-        LOG_INSTALL ON
-        )
-    
-    ExternalProject_Add_Step(
-        ${GOOGLE_PROTOBUF_PROJ_NAME}
-        ${GOOGLE_PROTOBUF_PROJ_NAME}_make_check
-        COMMENT "make check for google protobuf"
-        COMMAND make check
-        DEPENDEES build
-        DEPENDERS install
-        WORKING_DIRECTORY <BINARY_DIR>
-        )
+download_and_install_file(
+    ${GOOGLE_PROTOBUF_VERSION}
+    ${GOOGLE_PROTOBUF_VERSION_URL}
+    ${GOOGLE_PROTOBUF_VERSION_SHA1}
+    "${CMAKE_SOURCE_DIR}/.antilag-repo"
+    )
 
-    # Export Paths
-    set(GOOGLE_PROTOBUF_BINARY_PATH ${GOOGLE_PROTOBUF_PREFIX_DIR}/bin)
+ExternalProject_Add(
+    ${GOOGLE_PROTOBUF_PROJ_NAME}
+    PREFIX  ${GOOGLE_PROTOBUF_PREFIX_DIR}
+    DOWNLOAD_COMMAND ""
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --disable-shared
+    LOG_DOWNLOAD ON
+    LOG_UPDATE ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_TEST ON
+    LOG_INSTALL ON
+    )
 
-    set(GOOGLE_PROTOBUF_INCLUDE_PATH ${GOOGLE_PROTOBUF_PREFIX_DIR}/include)
+ExternalProject_Add_Step(
+    ${GOOGLE_PROTOBUF_PROJ_NAME}
+    ${GOOGLE_PROTOBUF_PROJ_NAME}_download_source
+    COMMENT "download source if it does not exist"
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${GOOGLE_PROTOBUF_PREFIX_DIR}/repo
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_SOURCE_DIR}/.antilag-repo/${GOOGLE_PROTOBUF_VERSION}" "${GOOGLE_PROTOBUF_PREFIX_DIR}/repo/${GOOGLE_PROTOBUF_VERSION}"
+    DEPENDERS download
+    WORKING_DIRECTORY <BINARY_DIR>
+    )
 
-    link_directories(${GOOGLE_PROTOBUF_PREFIX_DIR}/lib)
-    
-    # proto generation : source modified from MaidSafe as use-case was similar
-    function(generate_proto_src BaseName ProtoRootDir GeneratedProtoRootDir ProtoRelativeDir)
-    
-      set(${BaseName}ProtoSources "" PARENT_SCOPE)
-      set(${BaseName}ProtoHeaders "" PARENT_SCOPE)
-    
-      # Get list of .proto files
-      file(GLOB ProtoFiles RELATIVE ${ProtoRootDir} ${ProtoRootDir}/${ProtoRelativeDir}/*.proto)
+ExternalProject_Add_Step(
+    ${GOOGLE_PROTOBUF_PROJ_NAME}
+    ${GOOGLE_PROTOBUF_PROJ_NAME}_make_check
+    COMMENT "make check for google protobuf"
+    COMMAND make check
+    DEPENDEES build
+    DEPENDERS install
+    WORKING_DIRECTORY <BINARY_DIR>
+    )
 
-      # Search for and remove old generated .pb.cc and .pb.h files in the output dir
-      file(GLOB ExistingPbFiles
-           RELATIVE ${GeneratedProtoRootDir}
-           ${GeneratedProtoRootDir}/${ProtoRelativeDir}/*.pb.*)
-      list(LENGTH ExistingPbFiles ExistingPbFilesCount)
-      string(REGEX REPLACE "([^;]*)\\.proto" "\\1.pb.cc;\\1.pb.h" GeneratedFiles "${ProtoFiles}")
-      if(ExistingPbFilesCount)
+# Export Paths
+set(GOOGLE_PROTOBUF_BINARY_PATH ${GOOGLE_PROTOBUF_PREFIX_DIR}/bin)
+set(GOOGLE_PROTOBUF_INCLUDE_PATH ${GOOGLE_PROTOBUF_PREFIX_DIR}/include)
+
+link_directories(${GOOGLE_PROTOBUF_PREFIX_DIR}/lib)
+
+# proto generation : source modified from MaidSafe as use-case was similar
+function(generate_proto_src BaseName ProtoRootDir GeneratedProtoRootDir ProtoRelativeDir)
+
+    set(${BaseName}ProtoSources "" PARENT_SCOPE)
+    set(${BaseName}ProtoHeaders "" PARENT_SCOPE)
+
+    # Get list of .proto files
+    file(GLOB ProtoFiles RELATIVE ${ProtoRootDir} ${ProtoRootDir}/${ProtoRelativeDir}/*.proto)
+
+    # Search for and remove old generated .pb.cc and .pb.h files in the output dir
+    file(GLOB ExistingPbFiles
+        RELATIVE ${GeneratedProtoRootDir}
+        ${GeneratedProtoRootDir}/${ProtoRelativeDir}/*.pb.*)
+    list(LENGTH ExistingPbFiles ExistingPbFilesCount)
+    string(REGEX REPLACE "([^;]*)\\.proto" "\\1.pb.cc;\\1.pb.h" GeneratedFiles "${ProtoFiles}")
+    if(ExistingPbFilesCount)
         if(GeneratedFiles)
-          list(REMOVE_ITEM ExistingPbFiles ${GeneratedFiles})
+            list(REMOVE_ITEM ExistingPbFiles ${GeneratedFiles})
         endif()
         foreach(ExistingPbFile ${ExistingPbFiles})
-          file(REMOVE ${GeneratedProtoRootDir}/${ExistingPbFile})
-          message(STATUS "Removed ${ExistingPbFile}")
+            file(REMOVE ${GeneratedProtoRootDir}/${ExistingPbFile})
+            # message(STATUS "Removed ${ExistingPbFile}")
         endforeach()
-      endif()
-      
-      if(NOT ProtoFiles)
+    endif()
+
+    if(NOT ProtoFiles)
         return()
-      endif()
-    
-      # Set up protoc arguments
-      set(ProtocArgs "--proto_path=${ProtoRootDir}/${ProtoRelativeDir}")
-      set(ProtoImportDirs ${ARGN})
-      foreach(ProtoImportDir ${ProtoImportDirs})
+    endif()
+
+    # Set up protoc arguments
+    set(ProtocArgs "--proto_path=${ProtoRootDir}/${ProtoRelativeDir}")
+    set(ProtoImportDirs ${ARGN})
+    foreach(ProtoImportDir ${ProtoImportDirs})
         list(APPEND ProtocArgs "--proto_path=${ProtoImportDir}")
-      endforeach()
-      list(REMOVE_DUPLICATES ProtocArgs)
-      list(APPEND ProtocArgs "--cpp_out=${GeneratedProtoRootDir}/${ProtoRelativeDir}")
-      if(MSVC)
+    endforeach()
+    list(REMOVE_DUPLICATES ProtocArgs)
+    list(APPEND ProtocArgs "--cpp_out=${GeneratedProtoRootDir}/${ProtoRelativeDir}")
+    if(MSVC)
         list(APPEND ProtocArgs "--error_format=msvs")
-      else()
+    else()
         list(APPEND ProtocArgs "--error_format=gcc")
-      endif()
-    
-      message(STATUS "[generate_proto_src] BaseName=${BaseName}")
-      message(STATUS "[generate_proto_src] ProtoRootDir=${ProtoRootDir}")
-      message(STATUS "[generate_proto_src] GeneratedProtoRootDir=${GeneratedProtoRootDir}")
-      message(STATUS "[generate_proto_src] ProtoFiles=${ProtoFiles}")
+    endif()
 
-      # protoc exec path
-      # Add custom command to generate CC and header files
-      unset(GeneratedProtoFiles)
-      unset(GeneratedProtoSources)
-      unset(GeneratedProtoHeaders)
+    # message(STATUS "[generate_proto_src] BaseName=${BaseName}")
+    # message(STATUS "[generate_proto_src] ProtoRootDir=${ProtoRootDir}")
+    # message(STATUS "[generate_proto_src] GeneratedProtoRootDir=${GeneratedProtoRootDir}")
+    # message(STATUS "[generate_proto_src] ProtoFiles=${ProtoFiles}")
 
-      file(MAKE_DIRECTORY ${GeneratedProtoRootDir}/${ProtoRelativeDir})
-      foreach(ProtoFile ${ProtoFiles})
+    # protoc exec path
+    # Add custom command to generate CC and header files
+    unset(GeneratedProtoFiles)
+    unset(GeneratedProtoSources)
+    unset(GeneratedProtoHeaders)
+
+    file(MAKE_DIRECTORY ${GeneratedProtoRootDir}/${ProtoRelativeDir})
+    foreach(ProtoFile ${ProtoFiles})
         get_filename_component(ProtoFileNameWe ${ProtoFile} NAME_WE)
         set(GeneratedSource ${GeneratedProtoRootDir}/${ProtoRelativeDir}/${ProtoFileNameWe}.pb.cc)
         set(GeneratedHeader ${GeneratedProtoRootDir}/${ProtoRelativeDir}/${ProtoFileNameWe}.pb.h)
         list(APPEND GeneratedProtoSources "${GeneratedSource}")
         list(APPEND GeneratedProtoHeaders "${GeneratedHeader}")
         add_custom_command(OUTPUT ${GeneratedSource} ${GeneratedHeader}
-                           COMMAND ${GOOGLE_PROTOBUF_PREFIX_DIR}/bin/protoc ${ProtocArgs} ${ProtoRootDir}/${ProtoFile}
-                           DEPENDS ${GOOGLE_PROTOBUF_PREFIX_DIR}/bin/protoc ${ProtoRootDir}/${ProtoFile}
-                           COMMENT "Generated files from ${ProtoFileNameWe}.proto"
-                           VERBATIM)
-      endforeach()
-    
-      set_source_files_properties(${GeneratedProtoSources} ${GeneratedProtoHeaders} PROPERTIES GENERATED TRUE)
-      set_source_files_properties(${GeneratedProtoSources} PROPERTIES COMPILE_FLAGS "-w")
-    
-      set(${BaseName}ProtoSources ${GeneratedProtoSources} PARENT_SCOPE)
-      set(${BaseName}ProtoHeaders ${GeneratedProtoHeaders} PARENT_SCOPE)
-    
-    endfunction()
+            COMMAND ${GOOGLE_PROTOBUF_PREFIX_DIR}/bin/protoc ${ProtocArgs} ${ProtoRootDir}/${ProtoFile}
+            DEPENDS ${GOOGLE_PROTOBUF_PREFIX_DIR}/bin/protoc ${ProtoRootDir}/${ProtoFile}
+            COMMENT "Generated files from ${ProtoFileNameWe}.proto"
+            VERBATIM)
+    endforeach()
 
-endif()
+    set_source_files_properties(${GeneratedProtoSources} ${GeneratedProtoHeaders} PROPERTIES GENERATED TRUE)
+    set_source_files_properties(${GeneratedProtoSources} PROPERTIES COMPILE_FLAGS "-w")
+
+    set(${BaseName}ProtoSources ${GeneratedProtoSources} PARENT_SCOPE)
+    set(${BaseName}ProtoHeaders ${GeneratedProtoHeaders} PARENT_SCOPE)
+
+endfunction()
--- stubs/opt-googletest.cmake
+++ stubs/opt-googletest.cmake
@@ -1,83 +1,89 @@
 include(ExternalProject)
 
+set(GOOGLE_TEST_PROJ_NAME "GoogleTest")
+set(GOOGLE_TEST_PREFIX_DIR "${CMAKE_BINARY_DIR}/${GOOGLE_TEST_PROJ_NAME}")
+
 set(GOOGLE_TEST_VERSION "gtest-1.7.0.zip")
 set(GOOGLE_TEST_VERSION_SHA1 "f85f6d2481e2c6c4a18539e391aa4ea8ab0394af")
 set(GOOGLE_TEST_VERSION_URL "https://googletest.googlecode.com/files/${GOOGLE_TEST_VERSION}")
 
-if(ENABLE_TESTING)
+download_and_install_file(
+    ${GOOGLE_TEST_VERSION}
+    ${GOOGLE_TEST_VERSION_URL}
+    ${GOOGLE_TEST_VERSION_SHA1}
+    "${CMAKE_SOURCE_DIR}/.antilag-repo"
+    )
 
-    set(GOOGLE_TEST_PROJ_NAME "GoogleTest")
-    set(GOOGLE_TEST_PREFIX_DIR "${CMAKE_BINARY_DIR}/${GOOGLE_TEST_PROJ_NAME}")
-    
-    download_and_install_file(
-        ${GOOGLE_TEST_VERSION}
-        ${GOOGLE_TEST_VERSION_URL}
-        ${GOOGLE_TEST_VERSION_SHA1}
-        "${CMAKE_SOURCE_DIR}/.antilag-repo"
+ExternalProject_Add(
+    ${GOOGLE_TEST_PROJ_NAME}
+    PREFIX  ${GOOGLE_TEST_PREFIX_DIR}
+    DOWNLOAD_COMMAND ""
+    UPDATE_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_UPDATE ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_TEST ON
+    LOG_INSTALL ON
+    )
+
+ExternalProject_Add_Step(
+    ${GOOGLE_TEST_PROJ_NAME}
+    ${GOOGLE_TEST_PROJ_NAME}_download_source
+    COMMENT "download source if it does not exist"
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${GOOGLE_TEST_PREFIX_DIR}/repo
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_SOURCE_DIR}/.antilag-repo/${GOOGLE_TEST_VERSION}" "${GOOGLE_TEST_PREFIX_DIR}/repo/${GOOGLE_TEST_VERSION}"
+    DEPENDERS download
+    WORKING_DIRECTORY <BINARY_DIR>
+    )
+
+# Export Paths
+ExternalProject_Get_Property(${GOOGLE_TEST_PROJ_NAME} source_dir)
+set(GOOGLE_TEST_SOURCE_DIR ${source_dir})
+
+ExternalProject_Get_Property(${GOOGLE_TEST_PROJ_NAME} binary_dir)
+set(GOOGLE_TEST_BINARY_DIR ${binary_dir})
+
+link_directories(${GOOGLE_TEST_BINARY_DIR})
+
+# Enable Testing in Cmake
+enable_testing()
+
+# Functions
+function(add_cxx_test name src libs)
+    message(STATUS "[INFO] add_cxx_test : ${name}, ${src}, ${libs}")
+
+    # Executable Target
+    add_executable(${name} ${src})
+
+    # Dependent library compile and link resources
+    foreach (lib "${libs}")
+        get_property(
+            ${lib}_IncludeDirs
+            TARGET ${lib}
+            PROPERTY INCLUDE_DIRECTORIES
+            )
+        target_include_directories(${name} PRIVATE ${${lib}_IncludeDirs})
+
+        target_link_libraries(${name} ${lib})
+    endforeach()
+
+    # Dependent 'google-test' compile and link resources
+    target_include_directories(${name} PRIVATE ${GOOGLE_TEST_SOURCE_DIR}/include)
+
+    target_link_libraries(${name} 
+        gtest
+        gtest_main
+        # pthread
         )
-    
-    set(GOOGLE_TEST_VERSION_URL_REPO "file://${CMAKE_SOURCE_DIR}/.antilag-repo/${GOOGLE_TEST_VERSION}")
-    message(STATUS ${GOOGLE_TEST_VERSION_URL_REPO})
 
-    ExternalProject_Add(
-        ${GOOGLE_TEST_PROJ_NAME}
-        PREFIX  ${GOOGLE_TEST_PREFIX_DIR}
-        DOWNLOAD_DIR ${GOOGLE_TEST_PREFIX_DIR}/repo
-        URL ${GOOGLE_TEST_VERSION_URL_REPO}
-        URL_HASH SHA1=${GOOGLE_TEST_VERSION_SHA1}
-        # CMAKE_ARGS "${gtest_cmake_args}"
-        UPDATE_COMMAND ""
-        INSTALL_COMMAND ""
-        LOG_DOWNLOAD ON
-        LOG_CONFIGURE ON
-        LOG_BUILD ON
+    # Explicit dependency on googletest ExternalProject_Add
+    add_dependencies(${name} ${GOOGLE_TEST_PROJ_NAME})
+
+    # Add test instance
+    add_test(
+        NAME ${name} 
+        COMMAND ${name} "--gtest_break_on_failure"
         )
-    
-    ExternalProject_Get_Property(${GOOGLE_TEST_PROJ_NAME} source_dir)
-    set(GOOGLE_TEST_SOURCE_DIR ${source_dir})
-    
-    ExternalProject_Get_Property(${GOOGLE_TEST_PROJ_NAME} binary_dir)
-    set(GOOGLE_TEST_BINARY_DIR ${binary_dir})
-    
-    link_directories(${GOOGLE_TEST_BINARY_DIR})
-    
-    enable_testing()
-
-    function(add_cxx_test name src libs)
-      message(STATUS "[INFO] add_cxx_test : ${name}, ${src}, ${libs}")
-
-      # Executable Target
-      add_executable(${name} ${src})
-
-      # Dependent library compile and link resources
-      foreach (lib "${libs}")
-          get_property(
-              ${lib}_IncludeDirs
-              TARGET ${lib}
-              PROPERTY INCLUDE_DIRECTORIES
-              )
-          target_include_directories(${name} PRIVATE ${${lib}_IncludeDirs})
-
-          target_link_libraries(${name} ${lib})
-      endforeach()
-
-      # Dependent 'google-test' compile and link resources
-      target_include_directories(${name} PRIVATE ${GOOGLE_TEST_SOURCE_DIR}/include)
-
-      target_link_libraries(${name} 
-          gtest
-          gtest_main
-          # pthread
-          )
-
-      # Explicit dependency on googletest ExternalProject_Add
-      add_dependencies(${name} ${GOOGLE_TEST_PROJ_NAME})
-
-      # Add test instance
-      add_test(
-          NAME ${name} 
-          COMMAND ${name} "--gtest_break_on_failure"
-          )
-    endfunction()
-
-endif()
+endfunction()
